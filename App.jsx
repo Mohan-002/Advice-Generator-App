894e2fe772332b8b09b1238d705c4611b669f69d
Mohanapandian A <156403386+Mohan-002@users.noreply.github.com>
2024-02-25 21:10:29 +0530

    Add files via upload
    updated commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Advice from './Component/Advice'
+
+function App() {
+
+  return (
+    <>
+      <Advice />
+    </>
+  )
+}
+
+export default App
